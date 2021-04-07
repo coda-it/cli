b960d9339405aaca1e5254724769dc1538c23c18
Oskar Szura <oskar.szura@gmail.com>
2021-04-07 23:41:12 +0200

    CODA-160 - CLI rendered menu

--- core.h
+++ core.h
@@ -1,4 +1,5 @@
 #include "persistence.h"
+#include "types.h"
 #include <iostream>
 #include <map>
 #include <string>
@@ -6,62 +7,78 @@
 #include <vector>
 
 #define COMMAND_SEPARATOR " "
+#define KEY_UP 65
+#define KEY_DOWN 66
+#define KEY_ENTER 13
 
 class Cli {
 public:
+  int selected = 0;
+
   Cli() {
     this->persistence = new Persistence();
-    this->menu["add-deploy"] = &Cli::storeDeploy;
     this->menu["deploy"] = &Cli::deploy;
     this->menu["exit"] = &Cli::exit;
+  }
 
-    this->deployCommand = this->persistence->getAllRecords();
+  void render() {
+    // clear screen
+    std::cout << "\033[2J\033[1;1H";
+    std::cout << "=== CODA_ CLI ===\n\r\n\r";
+
+    int index = 0;
+    for (auto &&[key, value] : this->menu) {
+      if (index == this->selected) {
+        std::cout << "> " + key + "\n\r";
+      } else {
+        std::cout << "  " + key + "\n\r";
+      }
+
+      index++;
+    }
+    // erase cursor
+    std::cout << "\e[?25l";
   }
 
   void run() {
-    std::cout << "CODA_ CLI\n";
-    std::cout << "< ";
-    while (std::getline(std::cin, this->userInput)) {
-      this->handleInput(this->userInput);
-      std::cout << "< ";
-    }
+    system("stty raw");
+
+    int key;
+    do {
+      this->render();
+      std::cout << "key: " << key << "\n\r";
+      key = getchar();
+      this->handleInput(key);
+    } while (true);
   }
 
 private:
   Persistence *persistence;
   std::string userInput;
-  std::map<std::string, std::string> deployCommand;
-  std::map<std::string, void (Cli::*)(std::string)> menu;
+  std::map<std::string, void (Cli::*)()> menu;
 
-  void handleInput(std::string input) {
-    std::vector<std::string> inputVector =
-        coda::split(input, COMMAND_SEPARATOR);
-    std::string command = inputVector[0];
-
-    if (this->menu.find(command) != this->menu.end()) {
-      (this->*menu[command])(this->userInput);
+  void handleInput(char input) {
+    if (input == KEY_UP && this->selected > 0) {
+      this->selected -= 1;
+    } else if (input == KEY_DOWN && this->selected < this->menu.size() - 1) {
+      this->selected += 1;
+    } else if (input == KEY_ENTER) {
+      int index = 0;
+      for (auto &&[key, value] : this->menu) {
+        if (index == this->selected) {
+          (this->*menu[key])();
+        }
+        index++;
+      }
     }
   }
 
-  void storeDeploy(std::string input) {
-    std::vector<std::string> inputVector =
-        coda::split(input, COMMAND_SEPARATOR, 3);
-    std::cout << "> deployment added\n";
+  void deploy() {}
 
-    this->persistence->addRecord(inputVector[1], inputVector[2]);
-    this->deployCommand[inputVector[1]] = inputVector[2];
+  void exit() {
+    // clear screen
+    std::cout << "\033[2J\033[1;1H";
+    system("stty cooked");
+    ::exit(0);
   }
-
-  void deploy(std::string input) {
-    std::vector<std::string> inputVector =
-        coda::split(input, COMMAND_SEPARATOR);
-    std::string deployment = inputVector[1];
-
-    if (this->deployCommand.find(deployment) != this->deployCommand.end()) {
-      const char *cmd = this->deployCommand[deployment].c_str();
-      std::system(cmd);
-    }
-  }
-
-  void exit(std::string input) { ::exit(0); }
 };
