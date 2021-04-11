54ac7539b20ef2579d93ad89007c55b9a9523bc0
Oskar Szura <oskar.szura@gmail.com>
2021-04-11 20:31:07 +0200

    CODA-163 - Nested menu (#6)

--- types.h
+++ types.h
@@ -1,4 +1,13 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+class Cli;
+
+using Handler = void (Cli::*)();
+
+struct MenuItem {
+  std::map<std::string, MenuItem> child;
+  Handler handler;
+};
+
 #endif /* TYPES_H */
